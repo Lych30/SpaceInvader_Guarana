9ec15489a9f05fe6ee717ba2409cb198e77302d7
Leo <leopold.hillion@gmail.com>
2022-11-14 22:48:13 +0100

    test shake quand on tire

--- Space_Invader_Juicy/Assets/Scripts/ShootTest.cs
+++ Space_Invader_Juicy/Assets/Scripts/ShootTest.cs
@@ -2,19 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.ParticleSystemJobs;
+using Cinemachine;
 
 
 public class ShootTest : MonoBehaviour
 {
     public float Dmg;
     public ParticleSystem part;
+    CinemachineImpulseSource impulseSource;
     public List<ParticleCollisionEvent> collisionEvents;
 
     void Start()
     {
+     
+        impulseSource = GetComponent<CinemachineImpulseSource>();
         part = GetComponent<ParticleSystem>();
         collisionEvents = new List<ParticleCollisionEvent>();
     }
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            impulseSource.GenerateImpulse();
+            part.enableEmission = true;
+        }
+        else
+        {
+            part.enableEmission = false;
+        }
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         
@@ -32,6 +49,7 @@ public class ShootTest : MonoBehaviour
                 Vector3 force = collisionEvents[i].velocity*10;
                 other.GetComponent<EnnemyScript>().TakeDmg(Dmg,force);
                 
+
 
             }
             i++;
