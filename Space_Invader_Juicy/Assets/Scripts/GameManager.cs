3df650ecf73c07549e30a9ae2afba76818a974fd
MrEsport <lucasespositonc@gmail.com>
2022-11-15 16:09:52 +0100

    created GameManager

---
+++ Space_Invader_Juicy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+
+    public UnityEvent EnemyKilled = new UnityEvent();
+    public UnityEvent InvaderCountIncrement = new UnityEvent();
+
+    private int enemyCount = 0;
+
+    private void Awake()
+    {
+        if(instance != null)
+            Destroy(gameObject);
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    void Start()
+    {
+        EnemyKilled.AddListener(DecrementEnemyCount);
+        EnemyKilled.AddListener(CheckForEnemyLeft);
+    }
+
+    void Update()
+    {
+        // Effect Toggle Inputs
+    }
+
+    private void OnDestroy()
+    {
+        EnemyKilled.RemoveAllListeners();
+    }
+
+    public void Victory()
+    {
+    }
+
+    public void Defeat()
+    {
+    }
+
+    public void ReloadScene()
+    {
+    }
+
+    private void IncrementEnemyCount() => enemyCount++;
+    private void DecrementEnemyCount() => enemyCount--;
+
+    private void CheckForEnemyLeft()
+    {
+        if (enemyCount <= 0)
+            Victory();
+    }
+}
