723b2b17b21b5d33865bff0cd5be8ffa893b3b6d
MrEsport <lucasespositonc@gmail.com>
2022-11-24 10:08:42 +0100

    added VR grip and Joystick inputs

--- Space_Invader_Juicy/Assets/Scripts/VRInputManager.cs
+++ Space_Invader_Juicy/Assets/Scripts/VRInputManager.cs
@@ -4,8 +4,7 @@ using UnityEngine.InputSystem;
 
 public class VRInputManager : MonoBehaviour
 {
-    [Serializable]
-    public struct VRInput
+    [Serializable] public struct VRInput
     {
         [SerializeField] private InputActionReference inputRef;
 
@@ -13,12 +12,25 @@ public class VRInputManager : MonoBehaviour
         public bool GetButtonDown { get => inputRef.action.WasPressedThisFrame(); }
         public bool GetButtonUp { get => inputRef.action.WasReleasedThisFrame(); }
     }
+    [Serializable] public struct VRAxis
+    {
+        [SerializeField] private InputActionReference inputRef;
+
+        public Vector2 Direction { get => inputRef.action.ReadValue<Vector2>(); }
+        public float Horizontal { get => Direction.x; }
+        public float Vertical { get => Direction.y; }
+    }
 
     private static VRInputManager instance;
     public static VRInputManager Instance { get { return instance; } }
 
     public VRInput LeftTrigger;
     public VRInput RightTrigger;
+    public VRInput LefttGrip;
+    public VRInput RightGrip;
+
+    public VRAxis LeftStick;
+    public VRAxis RightStick;
 
     private void Awake()
     {
